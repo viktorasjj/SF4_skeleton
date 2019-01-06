655279264979bde4d7181537cae5d34fabfe0aa9
viktorasj <viktoras.jefimovas@gmail.com>
2019-01-06 22:56:12 +0200

    SF4_skeleton

---
+++ assets/js/app.js
@@ -0,0 +1,14 @@
+// yarn encore dev --watch
+
+require('../css/app.css');
+const $ = require('jquery');
+window.jQuery = $;
+
+// window.Tether = require('tether')
+// window.Popper = require('popper.js')
+// require('bootstrap');
+
+
+
+
+
